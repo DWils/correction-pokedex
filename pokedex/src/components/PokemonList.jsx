0ddeeae39a2b61719d471722e5ecd06a098455ac
David Wils <david.wils591@gmail.com>
2024-05-10 17:00:01 +0200

    mise en place de la recherche

--- pokedex/src/components/PokemonList.jsx
+++ pokedex/src/components/PokemonList.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 // PascalCase
 const PokemonList = () => {
 
     const [pokemons, setPokemons] = useState([]);
+    const [filteredPokemons, setFilteredPokemons] = useState([]);
+    const [keyWord, setKeyWord] = useState("");
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -15,29 +17,41 @@ const PokemonList = () => {
         })
     }, [])
 
-    const goToDetail = (id)=>{
+    const goToDetail = (id) => {
         navigate("/pokemon/" + id)
+    }
+
+    const searchByName = (keyWord) => {
+        setFilteredPokemons(pokemons.filter(pokemon => pokemon.name.toLowerCase().includes(keyWord)))
+        console.log(filteredPokemons);
     }
 
 
 
     return (
-        <div id="pokemon-list">
-            {pokemons.map((pokemon, index) => (
-                <div key={index} className='card'>
-                    <img src={pokemon.image} alt="pokemon-image" />
-                    <h2>{pokemon.name}</h2>
-                    <h4># {pokemon.pokedexId}</h4>
-                    <div>
-                        {pokemon.apiTypes.map((type, index) => (
-                            <p key={index}>{type.name}</p>
-                        ))}
-                    </div>
 
-                        <button onClick={()=>goToDetail(pokemon.id)}>Voir {pokemon.name}</button>
-                </div>
-            ))}
-        </div>
+        <>
+            <div id="search">
+                <input type="search" placeholder='saisissez un nom de pokémon' onChange={(e) => searchByName(e.target.value)}/>
+            </div>
+            <div id="pokemon-list">
+                {filteredPokemons.map((pokemon, index) => (
+                    <div key={index} className='card'>
+                        <img src={pokemon.image} alt="pokemon-image" />
+                        <h2>{pokemon.name}</h2>
+                        <h4># {pokemon.pokedexId}</h4>
+                        <div>
+                            {pokemon.apiTypes.map((type, index) => (
+                                <p key={index}>{type.name}</p>
+                            ))}
+                        </div>
+
+                        <button onClick={() => goToDetail(pokemon.id)}>Voir {pokemon.name}</button>
+                    </div>
+                ))}
+            </div>
+        </>
+
     )
 }
 
